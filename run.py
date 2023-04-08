05309bcc261761f62d924ff9fff46ee31b444783
nameiswaiyinsin <waiyinsin@gmail.com>
2023-04-08 17:19:40 +0000

    Added all html pages and inherited the content from base.html in our templates file, rendering from run.py in Flask

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)          #an instance of flask class and stored in variable called 'app'
@@ -7,11 +7,28 @@ app = Flask(__name__)          #an instance of flask class and stored in variabl
 
 @app.route("/")                #using the app.route decorator (@) also called pi notation, will be used to wrap functions as all functions are objects and can be passed around. When we try to browse to the root directory ("/"), the flask triggers the index() function and returns the hello world text.
 def index():
-    return "Hello, World"
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
 
 
 if __name__ == "__main__":
     app.run(
         host = os.environ.get("IP", "0.0.0.0"),
         port = int(os.environ.get("PORT", "5000")),
-        debug = True)
+        debug = True)           # debug = True should never be in a production project or when we submit for our assignments, it is a security flaw because it allows arbitrary codes to run. It can only be used in development stage of project. Change it to debug = False before submitting project.
+
+
